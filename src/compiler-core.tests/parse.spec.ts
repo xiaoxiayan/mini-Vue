ecff4f46ab7ab8bc5292d674082d5697da94794a
xiaoxiayan <a690150618@qq.com>
2022-03-16 20:55:50 +0800

    解析element

--- src/compiler-core.tests/parse.spec.ts
+++ src/compiler-core.tests/parse.spec.ts
@@ -5,7 +5,6 @@ describe('parse', () => {
   describe('interpolation', () => {
     test('simple interpolation', () => {
       const ast  = baseParse("{{ message}}");
-
       expect(ast.children[0]).toStrictEqual({
         type: NodeTypes.INTERPOLATION,
         content: {
@@ -13,6 +12,16 @@ describe('parse', () => {
           content: 'message'
         }
       })
+    })
+  })
+})
+
+describe('element', () => {
+  it('simple element div', () => {
+    const ast  = baseParse("<div></div>");
+    expect(ast.children[0]).toStrictEqual({
+        type: NodeTypes.ELEMENT,
+        tag: 'div'
     })
   })
 })
--- src/compiler-core.tests/src/parse.ts
+++ src/compiler-core.tests/src/parse.ts
@@ -1,4 +1,8 @@
 import { NodeTypes } from "./ast"
+const enum TagType {
+  Start,
+  End
+}
 
 export function baseParse(content:string) {
   const context = createParseContext(content)
@@ -20,8 +24,13 @@ function createRoot(children) {
 function parseChildren(context) {
   const nodes :any = []
   let node
-  if(context.sourse.startsWith('{{')){
+  const s = context.sourse
+  if(s.startsWith('{{')){
      node = parseInterpolation(context)
+  } else if (s[0] === '<') {
+    if (/[a-z]/i.test(s[1])) {
+      node = parseElement(context)
+    }
   }
   nodes.push(node)
   return  nodes
@@ -42,8 +51,6 @@ function parseInterpolation(context) {
   // 然后继续推进
 
   advanceBy(context, rawContentLength + closeDelimiter.length)
-
-  console.log('sourse==', context.sourse)
   return {
     type: NodeTypes.INTERPOLATION,
     content: {
@@ -56,3 +63,25 @@ function parseInterpolation(context) {
  function advanceBy(context: any, length: number) {
     context.sourse = context.sourse.slice(length)
  }
+
+function parseElement(context) {
+  const element = parseTag(context, TagType.Start)
+  parseTag(context, TagType.End)
+  console.log('-================', context.sourse)
+  return element
+}
+
+function parseTag(context: any, type: TagType) {
+    // 1.解析。tag 。正则
+    const match:any = /^<\/?([a-z]*)/i.exec(context.sourse)
+    const tag = match[1]
+    // 2.推进删除代码
+    advanceBy(context, match[0].length)
+    advanceBy(context, 1)
+    if(type === TagType.End) return
+    return {
+      type: NodeTypes.ELEMENT,
+      tag: tag
+    }
+}
+
