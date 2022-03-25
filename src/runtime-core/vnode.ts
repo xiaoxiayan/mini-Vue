64b5646e14e46ec219e15fd57e1eeda1d2f8c530
xiaoxiayan <a690150618@qq.com>
2022-03-25 19:48:20 +0800

    'add'

--- src/runtime-core/vnode.ts
+++ src/runtime-core/vnode.ts
@@ -10,7 +10,6 @@ export function createVNode(type, props?, children?) {
     shapeFlag: getShapFlag(type),
     children
   }
-  debugger
   if (typeof children === 'string') {
     vnode.shapeFlag |= ShapeFlags.TEXT_CHILDREN
   } else if (Array.isArray(children)) {
